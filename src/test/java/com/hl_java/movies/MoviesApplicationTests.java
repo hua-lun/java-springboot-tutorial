3aa1045ed37000a486c94553796a83e0c7fc7eae
hua-lun <chuahualun@gmail.com>
2024-07-07 20:34:26 +0800

    first commit

---
+++ src/test/java/com/hl_java/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hl_java.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
